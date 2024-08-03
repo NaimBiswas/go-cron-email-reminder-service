5551a536f8402b1f108fb9ac66b540fd724f926e
NaimBiswas <nayeembiswas2@gmail.com>
2024-08-03 14:58:22 +0600

    Cron Setup for Server Deployment

--- main.go
+++ main.go
@@ -16,16 +16,16 @@ func main() {
 
 	cron := cron.New()
 
-	schedule.Minute(cron)
-	fmt.Println("Minute Job has been started. Job Will Execute at every minute")
+	// schedule.Minute(cron, "* * * * *")
+	// fmt.Println("Minute Job has been started. Job Will Execute at every minute")
 
-	schedule.Hourly(cron)
-	fmt.Println("Hourly Job has been started. Job Will Execute at every hour")
+	// schedule.Hourly(cron, "0 * * * *")
+	// fmt.Println("Hourly Job has been started. Job Will Execute at every hour")
 
-	schedule.Daily(cron)
-	fmt.Println("Daily Job has been started. Job Will Execute at every day 12.00 AM")
+	// schedule.Daily(cron, "0 0 * * *")
+	// fmt.Println("Daily Job has been started. Job Will Execute at every day 12.00 AM")
 
-	schedule.Monthly(cron)
+	schedule.Monthly(cron, "00 07 7 * *")
 	fmt.Println("Monthly Job has been started. Job Will Execute at every month 5 11.30 AM")
 
 	cron.Start()
